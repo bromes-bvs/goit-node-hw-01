ff662b6866c3c175a77652f7704866d65d2e3357
Borys Supreme <bvs.bromes@gmail.com>
2023-02-03 16:57:17 +0200

    finished + add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+1. Получаем и выводим весь список контактов в виде таблицы (console.table)
+   Скриншот: https://ibb.co/VxZbmhK
+
+2. Получаем контакт по id
+   Скриншот: https://ibb.co/Dt2dnNN
+
+3. Добавялем контакт
+   Скриншот: https://ibb.co/LdYWWCS
+
+4. Удаляем контакт
+   Скриншот: https://ibb.co/svkvgkT
--- contacts.js
+++ contacts.js
@@ -7,52 +7,71 @@ const contactsPath = path.join(__dirname, './db/contacts.json');
 
 // TODO: задокументировать каждую функцию
 async function listContacts() {
-  const contactsList = await fs.readFile(contactsPath, 'utf-8');
-  return console.table(JSON.parse(contactsList));
+  try {
+    const contactsList = await fs.readFile(contactsPath, 'utf-8');
+    return console.table(JSON.parse(contactsList));
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function getContactById(contactId) {
-  const contactsList = await fs.readFile(contactsPath, 'utf-8');
+  try {
+    const contactsList = await fs.readFile(contactsPath, 'utf-8');
 
-  const searchContact = JSON.parse(contactsList).find(({ id }) => id === contactId.toString());
-  if (!searchContact) {
-    return console.log('Sorry there is no contact with this id');
+    const searchContact = JSON.parse(contactsList).find(({ id }) => id === contactId.toString());
+    if (!searchContact) {
+      return console.log('Sorry there is no contact with this id');
+    }
+    // У вигляді гарної таблиці
+    // console.table([searchContact]);
+
+    return console.table(searchContact);
+  } catch (error) {
+    console.log(error.message);
   }
-  return console.table(searchContact);
 }
 
 async function removeContact(contactId) {
-  const contactsList = await fs.readFile(contactsPath, 'utf-8');
-  const parsContacts = JSON.parse(contactsList);
-  const contactIndex = parsContacts.findIndex(({ id }) => id === contactId.toString());
-  // console.log(contactIndex);
-  if (contactIndex === -1) {
-    return console.log('Sorry there is no contact with this id');
+  try {
+    const contactsList = await fs.readFile(contactsPath, 'utf-8');
+    const parsContacts = JSON.parse(contactsList);
+    const contactIndex = parsContacts.findIndex(({ id }) => id === contactId.toString());
+    // console.log(contactIndex);
+    if (contactIndex === -1) {
+      return console.log('Sorry there is no contact with this id');
+    }
+
+    const deleteContact = parsContacts.splice(contactIndex, 1);
+
+    // console.table(parsContacts);
+    // Альтернативний варіант
+    // const updateList = parsContacts.filter(contact => contact.id !== contactId),
+
+    await fs.writeFile(contactsPath, JSON.stringify(parsContacts, null, '\t'));
+    return console.table(deleteContact);
+  } catch (error) {
+    console.log(error.message);
   }
-
-  const deleteContact = parsContacts.splice(contactIndex, 1);
-
-  // console.table(parsContacts);
-  // Альтернативний варіант
-  // const updateList = parsContacts.filter(contact => contact.id !== contactId),
-
-  await fs.writeFile(contactsPath, JSON.stringify(parsContacts, null, '\t'));
-  return console.table(deleteContact);
 }
 
 async function addContact(name, email, phone) {
-  const newContact = {
-    id: nanoid(4),
-    name,
-    email,
-    phone,
-  };
-  const contactsList = await fs.readFile(contactsPath, 'utf-8');
-  const contacts = JSON.parse(contactsList);
-  contacts.push(newContact);
+  try {
+    const newContact = {
+      id: nanoid(4),
+      name,
+      email,
+      phone,
+    };
+    const contactsList = await fs.readFile(contactsPath, 'utf-8');
+    const contacts = JSON.parse(contactsList);
+    contacts.push(newContact);
 
-  console.table(contacts);
-  return await fs.writeFile(contactsPath, JSON.stringify(contacts, null, '\t'));
+    console.table(contacts);
+    return await fs.writeFile(contactsPath, JSON.stringify(contacts, null, '\t'));
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 module.exports = {
